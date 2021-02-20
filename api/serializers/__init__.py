834ed45423667fa25c5ac091639dc8e3e667fd28
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-02-21 02:03:14 +0300

    Develop Title model, serializer and viewset

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -2,5 +2,5 @@ from .category_serializer import CategorySerializer
 from .email_auth_serializers import (EmailAuthSerializer,
                                      EmailCodePairSerializer)
 from .genre_serializer import GenreSerializer
-from .title_serializer import TitleSerializer
+from .title_serializer import TitleGetSerializer, TitlePostSerializer
 from .user_serializer import UserSerializer
