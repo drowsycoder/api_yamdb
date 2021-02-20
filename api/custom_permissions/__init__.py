b9ec9dc5bab1deff171e0498696e485820b084ab
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-02-21 02:09:05 +0300

    Solve problems with flake8 warnings

--- api/custom_permissions/__init__.py
+++ api/custom_permissions/__init__.py
@@ -1,2 +1,2 @@
-from .auth_permission import EmailCodeCorrectPermission
-from .is_admin import IsAdminRoleOrSuper
+from .auth_permission import EmailCodeCorrectPermission  # noqa
+from .is_admin import IsAdminRoleOrSuper  # noqa
