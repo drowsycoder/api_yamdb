da664b3ed15105ae45913671e8cb763931035093
Zaguzov <zaguzovalex@gmail.com>
2021-02-21 18:43:15 +0300

    Add permission for author

--- api/custom_permissions/is_author_or_higher.py
+++ api/custom_permissions/is_author_or_higher.py
@@ -6,5 +6,5 @@ class IsAuthorOrHigher(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         user = request.user
         return (request.method in permissions.SAFE_METHODS
-                or user.is_superuser or user.role == 'admin' or
+                or user.is_superuser or user.role in ['admin', 'moderator'] or
                 obj.author == user)
