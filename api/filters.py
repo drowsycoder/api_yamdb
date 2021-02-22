948b90c30b0df2d61adb78b1bb0072d150805ff3
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-02-22 22:53:17 +0300

    Add separate filter class for Title model

--- api/filters.py
+++ api/filters.py
@@ -1,12 +1,15 @@
-from django_filters import CharFilter, rest_framework as filters
+from django_filters.rest_framework import CharFilter, FilterSet, NumberFilter
 
 from .models import Title
 
 
-class NumberRangeFilter(filters.FilterSet):
-    genre = CharFilter(field_name='genre__slug', lookup_expr='iexact')
+class TitleFilter(FilterSet):
+    """Filters the titles by name, category, genre or year"""
+    name = CharFilter(field_name='name', lookup_expr='icontains')
     category = CharFilter(field_name='category__slug', lookup_expr='iexact')
+    genre = CharFilter(field_name='genre__slug', lookup_expr='iexact')
+    year = NumberFilter(field_name='year', lookup_expr='exact')
 
     class Meta:
         model = Title
-        fields = ['genre', 'category']
+        fields = ['name', 'category', 'genre', 'year']
