6eff40f29bfb07685438a1f26e9de3d473841964
Zaguzov <zaguzovalex@gmail.com>
2021-02-23 19:08:00 +0300

    Add index and descriptions for reviews and comments. Registered them in admin

--- api/custom_permissions/__init__.py
+++ api/custom_permissions/__init__.py
@@ -1,7 +1,7 @@
 from .is_admin import IsAdminRoleOrSuper
-from .is_author_or_higher import IsAuthorOrHigher
+from .is_author_or_higher import IsAuthorOrHigherOrReadOnly
 
 __all__ = [
     'IsAdminRoleOrSuper',
-    'IsAuthorOrHigher',
+    'IsAuthorOrHigherOrReadOnly',
 ]
