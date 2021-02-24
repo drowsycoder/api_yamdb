2785d57c08c71bc702e693325fb94d1a160ba62c
Zaguzov <zaguzovalex@gmail.com>
2021-02-24 19:02:24 +0300

    Fix title year validation import

--- api/validators/title_validator.py
+++ api/validators/title_validator.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 from django.core.exceptions import ValidationError
 
 
-def title_year_validator(year):
+def year_validator(year):
     """Проверяет значение года для произведения.
 
     Предполагается, что оно датировано с -1000 г. до н.э. по текущий год.
