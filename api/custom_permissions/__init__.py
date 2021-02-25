3877755121342309b3570daa6d88ebd23ff8cf93
Paul K <59116077+drowsycoder@users.noreply.github.com>
2021-02-25 12:41:55 +0300

    Reorder strings in __all__

--- api/custom_permissions/__init__.py
+++ api/custom_permissions/__init__.py
@@ -3,7 +3,7 @@ from .is_admin import IsAdminRoleOrSuperuser
 from .is_author_or_higher import IsAuthorOrHigherOrReadOnly
 
 __all__ = [
+    'BasicAccessPermission',
     'IsAdminRoleOrSuperuser',
     'IsAuthorOrHigherOrReadOnly',
-    'BasicAccessPermission',
 ]
